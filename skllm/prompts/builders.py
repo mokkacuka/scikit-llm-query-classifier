7fc74f86a518cc002e3f53526ee6a0279010b58d
mokkacuka <46848831+mokkacuka@users.noreply.github.com>
2023-07-02 22:09:31 +0200

    Update builders.py

--- skllm/prompts/builders.py
+++ skllm/prompts/builders.py
@@ -13,6 +13,91 @@ from skllm.prompts.templates import (
 
 # TODO add validators
 
+def build_zero_shot_prompt_slc_sc_dim1(
+    x: str, labels: str, template: str = ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM1
+) -> str:
+    """Builds a prompt for zero-shot single-label classification (SC Dimension 1).
+
+    Parameters
+    ----------
+    x : str
+        sample to classify
+    labels : str
+        candidate labels in a list-like representation
+    template : str
+        prompt template to use, must contain placeholders for all variables, by default ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM1
+
+    Returns
+    -------
+    str
+        prepared prompt
+    """
+    return template.format(x=x, labels=labels)
+
+def build_zero_shot_prompt_slc_sc_dim2(
+    x: str, labels: str, template: str = ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM2
+) -> str:
+    """Builds a prompt for zero-shot single-label classification (SC Dimension 2).
+
+    Parameters
+    ----------
+    x : str
+        sample to classify
+    labels : str
+        candidate labels in a list-like representation
+    template : str
+        prompt template to use, must contain placeholders for all variables, by default ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM2
+
+    Returns
+    -------
+    str
+        prepared prompt
+    """
+    return template.format(x=x, labels=labels)
+
+def build_zero_shot_prompt_slc_sc_dim3(
+    x: str, labels: str, template: str = ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM3
+) -> str:
+    """Builds a prompt for zero-shot single-label classification (SC Dimension 3).
+
+    Parameters
+    ----------
+    x : str
+        sample to classify
+    labels : str
+        candidate labels in a list-like representation
+    template : str
+        prompt template to use, must contain placeholders for all variables, by default ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM3
+
+    Returns
+    -------
+    str
+        prepared prompt
+    """
+    return template.format(x=x, labels=labels)
+
+def build_zero_shot_prompt_slc_sc_dim4(
+    x: str, labels: str, template: str = ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM4
+) -> str:
+    """Builds a prompt for zero-shot single-label classification (SC Dimension 4).
+
+    Parameters
+    ----------
+    x : str
+        sample to classify
+    labels : str
+        candidate labels in a list-like representation
+    template : str
+        prompt template to use, must contain placeholders for all variables, by default ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM4
+
+    Returns
+    -------
+    str
+        prepared prompt
+    """
+    return template.format(x=x, labels=labels)
+
+
 
 def build_zero_shot_prompt_slc_mu_dim1(
     x: str, labels: str, template: str = ZERO_SHOT_CLF_PROMPT_TEMPLATE_MU_DIM1
