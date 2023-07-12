3c0773d38b606bd6821f116202d2165983fcd9f1
mokkacuka <46848831+mokkacuka@users.noreply.github.com>
2023-07-12 11:31:04 +0200

    Update __init__.py

--- skllm/__init__.py
+++ skllm/__init__.py
@@ -9,6 +9,7 @@ from skllm.models.gpt_zero_shot_clf import (
     ZeroShotGPTClassifierScDimension2,
     ZeroShotGPTClassifierScDimension3,
     ZeroShotGPTClassifierScDimension4,
+    ZeroShotGPTClassifierScCategory,
 )
 # from skllm.models.gpt_few_shot_clf import FewShotGPTClassifier
 # from skllm.models.gpt_dyn_few_shot_clf import DynamicFewShotGPTClassifier
