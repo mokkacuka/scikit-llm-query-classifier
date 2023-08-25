012356daece7ffc9f601e0620d791da4f25a99eb
mokkacuka <46848831+mokkacuka@users.noreply.github.com>
2023-08-25 18:56:23 +0200

    Update __init__.py

--- skllm/__init__.py
+++ skllm/__init__.py
@@ -1,6 +1,5 @@
 # ordering is important here to prevent circular imports
 from skllm.models.gpt_zero_shot_clf import (
-    MultiLabelZeroShotGPTClassifier,
     ZeroShotGPTClassifierMuDimension1,
     ZeroShotGPTClassifierMuDimension2,
     ZeroShotGPTClassifierMuDimension3,
@@ -9,7 +8,5 @@ from skllm.models.gpt_zero_shot_clf import (
     ZeroShotGPTClassifierScDimension2,
     ZeroShotGPTClassifierScDimension3,
     ZeroShotGPTClassifierScDimension4,
-    ZeroShotGPTClassifierScCategory,
+    ZeroShotGPTClassifierScCategory
 )
-# from skllm.models.gpt_few_shot_clf import FewShotGPTClassifier
-# from skllm.models.gpt_dyn_few_shot_clf import DynamicFewShotGPTClassifier
