be36959af8a6403c998970a9e27e76fc6b3d2807
mokkacuka <46848831+mokkacuka@users.noreply.github.com>
2023-08-25 18:50:13 +0200

    Update templates.py

--- skllm/prompts/templates.py
+++ skllm/prompts/templates.py
@@ -1,162 +1,29 @@
-ZERO_SHOT_CLF_PROMPT_TEMPLATE_CATEGORY = """
-Act like a professional in the beauty industry.
+common_part1 = """Act like a professional in the {industry_or_category} industry.
 You will be provided with the following information:
-1. Search queries related to beauty. The list of search queries is delimited with triple backticks.
-2. List of product categories the search queries can be assigned to. The list is delimited with square brackets. The skin concerns in the list are enclosed in single quotes and comma separated.
+1. Search queries related to {industry_or_category}. The list of search queries is delimited with triple backticks.
+2. List of {items} the search queries can be assigned to. The list is delimited with square brackets. The {items} in the list are enclosed in single quotes and comma separated.
+"""
 
-Perform the following tasks:
-1. Identify the product category the provided search queries belong to with the highest probability. If the classification is not obvious, rather classify the category as *Unknown*.
-2. Assign the provided search query to that product category (e.g. "mascara": Makeup).
+common_part2 = """Perform the following tasks:
+1. Identify the {item} the provided search queries belong to with the highest probability. If the classification is not obvious, rather classify the {item} as {classification}.
+2. Assign the provided search query to that {item} (e.g. "{example}": {example_value}).
 3. Provide your response in a JSON format containing a single key `label` and a value corresponding to the assigned concern. Do not provide any additional information except the JSON.
-
-List of classes: {labels}
-
-Text sample: ```{x}```
-
+List of classes: {{labels}}
+Text sample: ```{{x}}```
 Your JSON response:
 """
 
+# Category dimension
+ZERO_SHOT_CLF_PROMPT_TEMPLATE_CATEGORY = common_part1.format(industry_or_category="beauty", items="product categories") + common_part2.format(item="product category", classification="*Unknown Category*", example="mascara", example_value="Makeup")
 
-ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM1 = """
-Act like a professional in the skincare industry.
-You will be provided with the following information:
-1. Search queries related to skincare. The list of search queries is delimited with triple backticks.
-2. List of skin concerns the search queries can be assigned to. The list is delimited with square brackets. The skin concerns in the list are enclosed in single quotes and comma separated.
+# Skincare dimensions
+ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM1 = common_part1.format(industry_or_category="skincare", items="skin concerns") + common_part2.format(item="skin concern", classification="*This query is not related to any skin concern known*", example="face cream", example_value="This query is not related to any skin concern known")
+ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM2 = common_part1.format(industry_or_category="skincare", items="skincare ingredients or features") + common_part2.format(item="skincare ingredient or feature", classification="*This query is not related to any skincare ingredient or feature known*", example="best vitamin c serum", example_value="Vitamin C")
+ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM3 = common_part1.format(industry_or_category="skincare", items="skincare products") + common_part2.format(item="skincare product", classification="*This query is not related to any skincare product known*", example="best vitamin c serum", example_value="Serum")
+ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM4 = common_part1.format(industry_or_category="skincare", items="skincare advices") + common_part2.format(item="skincare advice", classification="*This query is not related to any skincare advice*", example="best vitamin c serum", example_value="Best")
 
-Perform the following tasks:
-1. Identify the skin concern the provided search queries belong to with the highest probability. If the classification is not obvious, rather classify the beauty concern as *This query is not related to any skin concern known*.
-2. Assign the provided search query to that skin concern (e.g. "face cream": Unknown).
-3. Provide your response in a JSON format containing a single key `label` and a value corresponding to the assigned concern. Do not provide any additional information except the JSON.
-
-List of classes: {labels}
-
-Text sample: ```{x}```
-
-Your JSON response:
-"""
-
-ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM2 = """
-Act like a professional in the skincare industry.
-You will be provided with the following information:
-1. Search queries related to skincare. The list of search queries is delimited with triple backticks.
-2. List of skincare ingredients or features the search queries can be assigned to. The list is delimited with square brackets. The skincare ingredients and features in the list are enclosed in single quotes and comma separated.
-
-Perform the following tasks:
-1. Identify the skincare ingredient or feature the provided search queries belong to with the highest probability. If the ingredient or feature is not directly mentioned in the query, rather don't infer and classify the ingredient as *This query is not related to any skincare ingredient or feature known*.
-2. Assign the provided search query to that skincare ingredient or feature (e.g. "best vitamin c serum": Vitamin C).
-3. Provide your response in a JSON format containing a single key `label` and a value corresponding to the assigned concern. Do not provide any additional information except the JSON.
-
-List of classes: {labels}
-
-Text sample: ```{x}```
-
-Your JSON response:
-"""
-
-ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM3 = """
-Act like a professional in the skincare industry.
-You will be provided with the following information:
-1. Search queries related to skincare. The list of search queries is delimited with triple backticks.
-2. List of skincare products the search queries can be assigned to. The list is delimited with square brackets. The skincare products in the list are enclosed in single quotes and comma separated.
-
-Perform the following tasks:
-1. Identify the skincare product the provided search queries belong to with the highest probability. If the product is not directly mentioned in the query, rather don't infer and classify the ingredient as *This query is not related to any skincare product known*.
-2. Assign the provided search query to that skincare product (e.g. "best vitamin c serum": Serum).
-3. Provide your response in a JSON format containing a single key `label` and a value corresponding to the assigned concern. Do not provide any additional information except the JSON.
-
-List of classes: {labels}
-
-Text sample: ```{x}```
-
-Your JSON response:
-"""
-
-ZERO_SHOT_CLF_PROMPT_TEMPLATE_SC_DIM4 = """
-Act like a professional in the skincare industry.
-You will be provided with the following information:
-1. Search queries related to skincare. The list of search queries is delimited with triple backticks.
-2. List of skincare advices or inspirations the search queries can be assigned to. The list is delimited with square brackets. The skincare advices or inspirations in the list are enclosed in single quotes and comma separated.
-
-Perform the following tasks:
-1. Identify the skincare advice or inspiration the provided search queries belong to with the highest probability. If the classification is not obvious, rather classify the concern as *This query is not related to any beauty advice or inspiration*.
-2. Assign the provided search query to that skincare advice or inspiration (e.g. "best vitamin c serum": Best).
-3. Provide your response in a JSON format containing a single key `label` and a value corresponding to the assigned concern. Do not provide any additional information except the JSON.
-
-List of classes: {labels}
-
-Text sample: ```{x}```
-
-Your JSON response:
-"""
-
-ZERO_SHOT_CLF_PROMPT_TEMPLATE_MU_DIM1 = """
-Act like a professional in the makeup industry.
-You will be provided with the following information:
-1. Search queries related to makeup. The list of search queries is delimited with triple backticks.
-2. List of beauty concerns the search queries can be assigned to. The list is delimited with square brackets. The makeup concerns in the list are enclosed in single quotes and comma separated.
-
-Perform the following tasks:
-1. Identify the beauty concern the provided search queries belong to with the highest probability. If the classification is not obvious, rather classify the beauty concern as *This query is not related to any beauty concern*.
-2. Assign the provided search query to that concern (e.g. "lip gloss": Unknown).
-3. Provide your response in a JSON format containing a single key `label` and a value corresponding to the assigned concern. Do not provide any additional information except the JSON.
-
-List of classes: {labels}
-
-Text sample: ```{x}```
-
-Your JSON response:
-"""
-
-ZERO_SHOT_CLF_PROMPT_TEMPLATE_MU_DIM2 = """
-Act like a professional in the makeup industry.
-You will be provided with the following information:
-1. Search queries related to makeup. The list of search queries is delimited with triple backticks.
-2. List of makeup ingredients or features the search queries can be assigned to. The list is delimited with square brackets. The ingredients and features in the list are enclosed in single quotes and comma separated.
-
-Perform the following tasks:
-1. Identify the makeup ingredient or feature the provided search queries belong to with the highest probability. If the classification is not obvious, rather classify the makeup ingredient or feature as *This query is not related to any makeup ingredient or feature known*.
-2. Assign the provided search query to that ingredient or feature (e.g. "cream with hyaluronic acid": Hyaluronic Acid).
-3. Provide your response in a JSON format containing a single key `label` and a value corresponding to the assigned concern. Do not provide any additional information except the JSON.
-
-List of classes: {labels}
-
-Text sample: ```{x}```
-
-Your JSON response:
-"""
-
-ZERO_SHOT_CLF_PROMPT_TEMPLATE_MU_DIM3 = """
-Act like a professional in the makeup industry.
-You will be provided with the following information:
-1. Search queries related to makeup. The list of search queries is delimited with triple backticks.
-2. List of makeup products the search queries can be assigned to. The list is delimited with square brackets. The products in the list are enclosed in single quotes and comma separated.
-
-Perform the following tasks:
-1. Identify the makeup product the provided search queries belong to with the highest probability. If the classification is not obvious, rather classify the makeup product as *This query is not related to any makeup product known*.
-2. Assign the provided search query to that product (e.g. "plumper lips": Unknown).
-3. Provide your response in a JSON format containing a single key `label` and a value corresponding to the assigned concern. Do not provide any additional information except the JSON.
-
-List of classes: {labels}
-
-Text sample: ```{x}```
-
-Your JSON response:
-"""
-
-ZERO_SHOT_CLF_PROMPT_TEMPLATE_MU_DIM4 = """
-Act like a professional in the makeup industry.
-You will be provided with the following information:
-1. Search queries related to makeup. The list of search queries is delimited with triple backticks.
-2. List of makeup advice or inspirations the search queries can be assigned to. The list is delimited with square brackets. The advice or inspirations in the list are enclosed in single quotes and comma separated.
-
-Perform the following tasks:
-1. Identify the makeup advice or inspiration the provided search queries belong to with the highest probability. If the classification is not obvious, rather classify the makeup advice or inspiration as *This query is not related to any makeup advice or inspiration known*.
-2. Assign the provided search query to that makeup advice or inspiration (e.g. "blush": Unknown).
-3. Provide your response in a JSON format containing a single key `label` and a value corresponding to the assigned concern. Do not provide any additional information except the JSON.
-
-List of classes: {labels}
-
-Text sample: ```{x}```
-
-Your JSON response:
-"""
+# Makeup dimensions
+ZERO_SHOT_CLF_PROMPT_TEMPLATE_MU_DIM1 = common_part1.format(industry="makeup", items="beauty concerns") + common_part2.format(item="beauty concern", classification="*This query is not related to any beauty concern known*", example="lip gloss", example_value="This query is not related to any beauty concern known")
+ZERO_SHOT_CLF_PROMPT_TEMPLATE_MU_DIM2 = common_part1.format(industry="makeup", items="ingredients or features") + common_part2.format(item="ingredient or feature", classification="*This query is not related to any makeup ingredient or feature known*", example="waterproof mascara", example_value="Waterproof")
+ZERO_SHOT_CLF_PROMPT_TEMPLATE_MU_DIM3 = common_part1.format(industry="makeup", items="makeup products") + common_part2.format(item="makeup product", classification="*This query is not related to any makeup product known*", example="red lipstick", example_value="Lipstick")
+ZERO_SHOT_CLF_PROMPT_TEMPLATE_MU_DIM4 = common_part1.format(industry="makeup", items="makeup advices or inspiration") + common_part2.format(item="makeup advice or inspiration", classification="*This query is not related to any makeup advice or inspiration known*", example="best red lipstick", example_value="Best")
